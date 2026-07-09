d69cf5650e57e4ff7194febdb9942eda642952fd
sangam kunwar <info@sangamkunwar.com.np>
2026-07-09 05:45:15 +0300

    Update BASE_URL for sitemap route

--- src/routes/sitemap[.]xml.ts
+++ src/routes/sitemap[.]xml.ts
@@ -1,7 +1,7 @@
 import { createFileRoute } from "@tanstack/react-router";
 import type {} from "@tanstack/react-start";
 
-const BASE_URL = "https://devpulse-nexus.lovable.app";
+const BASE_URL = "https://devpulse.sangamkunwar.com.np";
 
 export const Route = createFileRoute("/sitemap.xml")({
   server: {
